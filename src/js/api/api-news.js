699dfc1f7e33d1bbf8e9d5afa791e45143cc37b1
Vitaly Nelyub <nelyub86@ukr.net>
2023-03-01 21:08:05 +0200

    test

--- src/js/api/api-news.js
+++ src/js/api/api-news.js
@@ -1,5 +1,5 @@
 // const KEY = '1CftshpIbKCwUhOxVLAbMSPcGc2N2XMQ';
 
 // ---------------------------IMPORT------------------------------------
-import axios from 'axios';
+// import axios from 'axios';
 
