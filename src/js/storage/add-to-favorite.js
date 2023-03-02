dbb15db1ecd01dee2f26b0da98b51e8885a6b1ef
Lyubov16 <lyubov.vaskivna@gmail.com>
2023-03-02 19:38:48 +0200

    add

---
+++ src/js/storage/add-to-favorite.js
@@ -0,0 +1,15 @@
+
+
+// const errorPage = document.querySelector('.error');
+// const favoritePage = document.querySelector('.favorite__section')
+
+// const favouriteNews = localStorage.getItem('news');
+// // console.log(favouriteNews);
+// const parseNews = JSON.parse(favouriteNews)
+// // console.log(parseNews);
+
+// if (!parseNews) {
+//     errorPage.classList.remove('is-hidden');
+//     favoritePage.classList.add('is-hidden');
+//     return
+// }
