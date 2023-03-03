d339d60245a6a188abd32ed074d318913d64c5a0
YuliiaMay <110532663+YuliiaMay@users.noreply.github.com>
2023-03-03 19:26:12 +0000

    Deploying to gh-pages from @ YuliiaMay/project-js-07@92c1236ef7aadca2c3ac1b23af7c4a68d05caebe 🚀

---
+++ read.74c6147e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".read__list-date");console.log(e);const o=document.querySelector(".read__list");console.log(o);
+//# sourceMappingURL=read.74c6147e.js.map
---
+++ read.84774241.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".read__list-date");console.log(e);var o=document.querySelector(".read__list");console.log(o)}();
+//# sourceMappingURL=read.84774241.js.map
