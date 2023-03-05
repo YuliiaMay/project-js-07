8f5115f63ed44331ef10b6ba87ead820b33ad7ab
YuliiaMay <110532663+YuliiaMay@users.noreply.github.com>
2023-03-05 12:54:42 +0000

    Deploying to gh-pages from @ YuliiaMay/project-js-07@92c0a9824721b96b80b0f9b4ebefc4ab04cf7b43 🚀

---
+++ favorite.a72a8706.js
@@ -0,0 +1,2 @@
+!function(){var n,e,t;document.body.onload=function(){setTimeout((function(){var n=document.getElementById("page-preloader");n.classList.contains("done")||n.classList.add("done")}),1e3)};n=!1,e=document.querySelectorAll(".nav__link"),t=document.URL,e.forEach((function(e){t.includes(e.pathname)&&(e.classList.add("nav__link--current"),n=!0)})),!n&&document.querySelector('.nav__link[href*="/index.html"]').classList.add("nav__link--current")}();
+//# sourceMappingURL=favorite.a72a8706.js.map
