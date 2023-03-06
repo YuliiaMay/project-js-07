bf17ca6237392ecccaae0648963ea05177dff085
YuliiaMay <110532663+YuliiaMay@users.noreply.github.com>
2023-03-06 20:55:11 +0000

    Deploying to gh-pages from @ YuliiaMay/project-js-07@339e4d6eeaef4887d175c0d63a3b9434b71340e5 🚀

--- favorite.234c3efe.js
+++
@@ -1,2 +0,0 @@
-document.body.onload=function(){setTimeout((function(){let n=document.getElementById("page-preloader");n.classList.contains("done")||n.classList.add("done")}),1e3)};!function(){let n=!1;const e=document.querySelectorAll(".nav__link"),t=document.URL;e.forEach((e=>{t.includes(e.pathname)&&(e.classList.add("nav__link--current"),n=!0)})),!n&&document.querySelector('.nav__link[href*="/index.html"]').classList.add("nav__link--current")}();
-//# sourceMappingURL=favorite.234c3efe.js.map
