be3af5533bbec56cd0f17186796bf2c17d68bfe2
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-03-07 12:17:58 +0200

    read-storage

--- src/js/storage/add-to-read.js
+++ src/js/storage/add-to-read.js
@@ -1,144 +1,80 @@
-import {showEmptyResponse} from './storage-read';
-import { READ_NEWS_KEY } from "../api/create-card";
+import axios from 'axios';
+import { getDataFromLocalStorage, setDataToLocalStorage } from '../api/service';
+// import { FILTERED_NEWS_URL } from '../main/filter-category'
+// import { fetchPopularNews } from '../storage/add-to-favorite'
 
-// const gallery = document.querySelector('.news__gallery');
 
-// export const FAV_NEWS_KEY = 'read-news';
-const cardsReadArray = [];
-const savedReadCard = {};
+const READ_NEWS_KEY = 'read-news';
+
+const gallery = document.querySelector('.news__gallery');
+
+// const uniqReadCardArr = [];
+let readData = null;
 let cardId = null;
 
-// gallery.addEventListener('click', onReadClick);
 
 
 
+// const FILTERED_NEWS_URL = `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=ctrAXxxlZTZKIuOVxETyJyELWuuMaa5A`;
+const POPULAR_NEWS_URL = `https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=ctrAXxxlZTZKIuOVxETyJyELWuuMaa5A`;
 
+async function getPopularNews() {
+    let { data } = await axios.get(POPULAR_NEWS_URL);
+    return data;
+}
 
+// async function getFilteredNews() {
+//     let { data } = await axios.get(FILTERED_NEWS_URL);
+//     return data;
+// }
 
-
-export function getParsedData(READ_NEWS_KEY) {
-    try {
-        const data = localStorage.getItem(READ_NEWS_KEY);
-        console.log(111);
-        return data === null
-            ? undefined
-            : JSON.parse(data);
-        
-    } catch (error) {
-        showEmptyResponse();
+// перевірка чи клік у межах галереї статтей   
+if (gallery) {
+    gallery.addEventListener('click', onReadMoreClick);
+}
+    
+async function onReadMoreClick(e) {
+    if (e.target.nodeName !== 'A') {
+        return;
     }
+    
+    const uniqReadCardArr = getDataFromLocalStorage(READ_NEWS_KEY);
+
+
+    const cardId = e.target.closest('.card').dataset.id;
+    
+
+    const { results } = await getPopularNews();
+    // const { results } = await getFilteredNews();
+    readData = results;
+
+    const uniqReadCard = readData.find(({ id }) => Number(id) === Number(cardId));
+
+
+    if (uniqReadCard !== undefined) {
+        if (uniqReadCardArr.every(uniqReadCard => Number(cardId) !== Number(uniqReadCard.id))) {
+            uniqReadCardArr.push(uniqReadCard);
+        }
+    }
+
+    setDataToLocalStorage(READ_NEWS_KEY, uniqReadCardArr);  
 }
 
 
 
 
-export function addEventsOnCard() {
-    const btnsReadMore = document.querySelectorAll('.js-read-news-btn');
-    const gallery = document.querySelectorAll('.news__container');
-    console.log(btnsReadMore);
-    console.log(gallery);
-}
 
 
 
-
-
-
-// function getRead() {
-    
-//     const savedReadCard = JSON.parse(localStorage.getItem('READ_NEWS_KEY')) || {};
-//     return savedReadCard;
+// 000000000000000000000000000000000000000
+// async function getFilteredNews() {
+//     let { data } = await axios.get(FILTERED_NEWS_URL);
+//     console.log(data);
+//     return data;
 // }
 
-// function onReadClick(e) {
-//     if (e.target.nodeName !== 'A') {
-//         return;
-//     }
-
-//     const btnReadMore = e.target;
-//     console.log(btnReadMore);
-//     cardId = e.target.closest('.card').dataset.id;
-
-//     getPopularCardData(cardId);
+// async function getFoundNews() {
+//     let { data } = await axios.get(POPULAR_NEWS_URL);
+//     console.log(data);
+//     return data;
 // }
-
-async function getPopularCardData(cardId) {
-    // console.log(cardId);
-    // console.log(data);
-    let response = await axios.get(
-        'https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=ctrAXxxlZTZKIuOVxETyJyELWuuMaa5A'
-    );
-
-    const data = response.data.results;
-
-    
-
-        data.map(
-    ({ media, source, title, abstract, published_date, url, section, id }) => {
-        const savedReadCard = {};
-        
-        console.log(savedReadCard);
-
-        let src = media.length
-            ? media.map(media => media['media-metadata'][2].url)
-            : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvFBa3G11OUBYADP7ouSBgwiiRzSYorF4dfg&usqp=CAU';
-
-        savedReadCard['media'] = `${media}`;
-        savedReadCard['source'] = `${source}`;
-        savedReadCard['title'] = `${title}`;
-        savedReadCard['section'] = `${section}`;
-        savedReadCard['abstract'] = `${abstract}`;
-        savedReadCard['published_date'] = `${published_date}`;
-        savedReadCard['src'] = `${src}`;
-        savedReadCard['id'] = `${id}`;
-
-
-        if (cardsReadArray.every(savedReadCard => Number(savedReadCard['id']) !== Number(cardId))) {
-            // cardsReadArray.push(savedReadCard);
-            cardsReadArray.push(savedReadCard);
-        };
-    });
-
-            // console.log(data);
-}
-
-// function saveData(data) {
-//     // getPopularCardData();
-
-//             // if (savedReadData.every(savedReadCard => Number(savedReadCard.id) !== Number(id)))
-//                 // cardsReadArray.push(savedReadCard);
-            
-//             // console.log(savedReadCard);
-//             // console.log(cardsReadArray);
-
-//             // wrightReadCardById(savedReadCard['id'])
-            
-//             // localStorage.setItem(READ_NEWS_KEY, JSON.stringify(cardsReadArray));
-//             // console.log(read);
-//         }
-// }
-
-
-
-// function wrightReadCardById(id) {
-//     const readCard = cardsReadArray.find(savedReadCard => savedReadCard.id === id);
-    
-
-//     // localStorage.setItem(READ_NEWS_KEY, JSON.stringify(savedReadData));
-
-
-//     // const savedReadData = JSON.parse(localStorage.getItem(READ_NEWS_KEY));
-
-//     // if (!savedReadData) {
-//     //     const readCardArray = [];
-//     //     readCardArray.push(readCard);
-//     //     localStorage.setItem(READ_NEWS_KEY, JSON.stringify(readCardArray));
-//     //     console.log('-');
-//     // }
-//     // else if (savedReadData.every(savedReadCard => Number(savedReadCard.id) !== Number(id))) {
-//     //     console.log('+');
-//     //     savedReadData.push(readCard);
-//     //     localStorage.setItem(READ_NEWS_KEY, JSON.stringify(storageNews));
-//     // }
-    
-//     };
--- src/js/storage/save-data.js
+++ src/js/storage/save-data.js
@@ -1,64 +1,21 @@
-import axios from 'axios';
-// import NewsApiService from '../api/api-news';
-// import { fetchNews, fetchPopularNews, renderCategoryCard } from "../gallery";
 
 
-// const newsApiService = new NewsApiService();
-// fetchPopularNews();
-// saveData(data);
-getPopularCardData();
-
-async function getPopularCardData() {
-    let response = await axios.get('https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=ctrAXxxlZTZKIuOVxETyJyELWuuMaa5A');
-
-    const data = response.data.results;
-    
-}
-
-// const ar
-const cardsFavArray = [];
 
 
-function saveData(data) {
-    const savedFavCard = {};
-    data.map(({ media, source, title, abstract, published_date, url, section, id }) => {
-    
-    let src = media.length
-        ? media.map(media => media['media-metadata'][2].url)
-        : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvFBa3G11OUBYADP7ouSBgwiiRzSYorF4dfg&usqp=CAU';
-    
-    savedFavCard['media'] = `${media}`;
-    savedFavCard['source'] = `${url}`;
-    savedFavCard['title'] = `${title}`;
-    savedFavCard['section'] = `${section}`;
-    savedFavCard['abstract'] = `${abstract}`;
-    savedFavCard['published_date'] = `${published_date}`;
-    savedFavCard['src'] = `${src}`;
-    savedFavCard['src'] = `${id}`;
-        
-        
-    cardsFavArray.push(savedFavCard);   
-    // console.log(savedFavCar);   
-});
-}
-
-
-const FAVORITE_KEY = 'favorite';
-// const READ_KEY = 'favorite';
-
-
-// const galleryEl = document.querySelector('.news__gallery'); 
-// const cardEl = document.querySelector('.card'); 
-
-
-// galleryEl.addEventListener('click', onFavBtnClick); 
-
-// function onFavBtnClick(e) {
-//     const btnFav = e.target;
-
-//     if (e.target.nodeName !== 'BUTTON') {
-//         return
-//     }
-
-//     console.log(cardsFavArray);
+// export function getDataFromLocalStorage(key) {
+//   try {
+//     const data = localStorage.getItem(key);
+//     return data ? JSON.parse(data) : [];
+//   } catch (error) {
+//     console.log(error.message);
+//   }
 // }
+
+// export function setDataToLocalStorage(key, data) {
+//   localStorage.setItem(key, JSON.stringify(data));
+// }
+
+
+
+
+
