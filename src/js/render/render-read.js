a9adb55bf268fb35298eccfac4361d97583ca20c
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-03-08 21:40:37 +0200

    -

--- src/js/render/render-read.js
+++ src/js/render/render-read.js
@@ -1,121 +1,160 @@
 let savedReadsCardArr = [];
 
 import { getDataFromLocalStorage } from '../api/service';
-import { onReadMoreClick } from '../storage/add-to-read';
-
-import { fetchNews } from '../gallery';
-
-const newsContainer = document.querySelector('.accordion');
-const undefinedRef = document.querySelector('.undefined');
-
-
-
-// // containerRef.addEventListener('click', onReadMoreClick);
-// containerRef.addEventListener('click', e => {
-//   // const link = e.target.closest('.card__ref');
-//   if (!link) return;
-//   link.closest('.card').remove();
-//   init();
-// });
-
+// import { onReadMoreClick } from '../storage/add-to-read';
 import { READ_NEWS_KEY } from '../storage/add-to-read';
+// import { fetchNews } from '../gallery';
+// import { Loading } from 'notiflix';
 
-function chooseReadMarkup() {
+
+
+const iconPath = new URL('/src/images/icons.svg', import.meta.url);
+const accContainer = document.querySelector('.acc__container');
+const emptyContainer = document.querySelector('.empty-response__container');
+const accGallery = document.querySelector('.acc__gallery');
+
+let dates = [];
+let uniqAccordionDays = [];
+
+
+let sameDayRead = [];
+
+let readMarkupCard = '';
+
+switchReadMarkup();
+
+
+
+createAcc(uniqAccordionDays, readMarkupCard);
+getReadCard(savedReadsCardArr);
+
+
+
+
+
+
+function switchReadMarkup() {
   try {
     savedReadsCardArr = getDataFromLocalStorage(READ_NEWS_KEY);
-
+    
     if (savedReadsCardArr.length !== 0) {
-      undefinedRef.classList.add('is-hidden');
-      newsContainer.classList.remove('is-hidden');
-			fetchNews(savedReadsCardArr);
-			getUniqDayArr(savedReadsCardArr);
+      emptyContainer.classList.add('is-hidden');
+      accContainer.classList.remove('is-hidden');
+      getUniqueDaysOfReading(savedReadsCardArr);
 
+      setReadCardsByDate(savedReadsCardArr, uniqAccordionDays);
+      // createAcc(uniqAccordionDays);
+
+
+      // getReadCard(savedReadsCardArr);
+      // createAccordions(uniqAccordionDays);
+      // createAccordionMarkup(uniqAccordionDays, savedReadsCardArr);
+			// fetchNews(savedReadsCardArr);
     }
   } catch (error) {
     console.log(error.message);
   }
 }
 
-chooseReadMarkup();
-console.log(savedReadsCardArr);
 
-function getUniqDayArr(savedReadsCardArr) {
-  let dates = [];
-  console.log(savedReadsCardArr);
+function getUniqueDaysOfReading(savedReadsCardArr) {
   savedReadsCardArr.map(card => {
-    for (day in card) {
-      let hasSameDay = card.includes(day);
-      if (!hasSameDay) {
-        dates.push(day);
-      }
-    }
-  });
+    const newDay = card.day;
+    dates.push(newDay);
+  })
+  
+  // масив дат для контейнерів аккардеону
+  uniqAccordionDays = Array.from(new Set(dates));
+  console.log(uniqAccordionDays);
+  return uniqAccordionDays;
 }
 
-// savedReadsCardArr.every(card => card.date !== )
 
-// accordionHeaders.forEach(header => {
 
-//   header.addEventListener('click', () => {
-//     const accordionItem = header.parentElement;
-//     accordionItem.classList.toggle('active');
-//     const accordionContent = accordionItem.querySelector('.accordion-content');
-//     accordionContent.classList.toggle('active');
-//   });
-// });
+function setReadCardsByDate(savedReadsCardArr, uniqAccordionDays) {
+  for (const card of savedReadsCardArr) {
+    console.log(card.day);
+    for (const day of uniqAccordionDays) {
+      
+      sameDayRead.push(card);
+    }
+  }
+}
 
-// renderReadNews();
 
-// const searchBtn = document.querySelector('.search__btn');
-// searchBtn.addEventListener('click', getSearchQueryFromRead);
-// function getSearchQueryFromRead() {
-// }
+function createAcc(savedReadsCardArr) {
 
-// function updateUI(searchNews) {
-//   const markup = createMarkup(search);
-//   readNewsContainer.innerHTML = markup;
-//   addEventHandlers();
-// }
+  for (const day of savedReadsCardArr) {
+  const accMarckup = savedReadsCardArr
+        .map(el => {
+          return `<div class="acc-item">
+                    <div class="acc__date-btn">08.03.2023</div>
+                  </div>`
+        })
+        .join('');
+      
+    accContainer.insertAdjacentHTML('afterbegin', accMarckup);
+    return accMarckup;
+    }
+      
+  }
 
-// function renderReadNews() {
-//   // const parsedReadData = getParsedData(READ_NEWS_KEY);
-//   // console.log(parsedReadData);
 
-//   // if (!parsedReadData) {
-//   //     showEmptyResponse();
-//   //     return;
-//   // }
 
-//   // // фільтруємо унікальні картки
-//   // const savedReadsCardArr = parsedReadData.filter(card => card.read === true);
 
-//   // if (savedReadsCardArr.length < 1) {
-//   //     showEmptyResponse();
-//   //     return;
-//   // }
-//   return;
-// }
+function getReadCard(savedReadsCardArr) {
+  // for (const day of uniqAccordionDays) {
+  //   console.log(day);
 
-// function createMarkup(cards) {
-//   const date = cards.map(card => card.dayRead);
+  readMarkupCard = savedReadsCardArr
+    .map(({ media, source, title, abstract, published_date, url, section, id }) => {
+      let src = media.map(media => media['media-metadata'][2].url);
+      console.log(src);
+        
+      return `<div class="card" data-id="${id}">
+                <div class="wrap__img">
+                  <img class="card__img is-reading" src="${media}" alt="${source}" />
+                  <button type="button" class="item-news__add-to-favorite>
+                  <span class="item-news__add-to-favorite-btn">
+                    Add to favorite
+                      <svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
+                              <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path>
+                      </svg>
+                      </span>
+                              <span class="item-news__remove-to-favorite-btn">Remove from favorite
+                      <svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
+                              <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path>
+                      </svg></span>
+                  </button>
+                </div>
+                <div class="news__card">
+                <div class="news__card--info">
+                <h2 class="card__title">${title.substr(0, 55)}</h2>
+                <p class="card__description">${abstract.substr(0, 100) + '...'
+        }</p>
+                </div>
+                <div class="card__footer">
+                  <span class="card__date">${published_date}</span>
+                  <a class="card__ref" target="_blank"
+                  rel="noreferrer noopener">Read more</a>
+                </div>
+                </div>
+                <div class="categories">${section}</div>
 
-//   if (date.length < 1) {
-//     showEmptyResponse();
-//   }
+                <div class="read visually-hidden">
+                  <span class="read__main">Already read</span>
+                  <svg class="read__main-icon">
+                      <use class="icon" href="${iconPath}#done""></use>
+                  </svg>
+                </div>                
+              </div>`
+      })
+          .join('');
 
-//   let markup = '';
-//   const uniqDateArr = Array.from(new Set(date));
-//   const filteredDateArr = uniqDateArr.filter(date => date !== undefined);
-//   const sortedDateArr = filteredDateArr.sort((a, b) => b.localeCompare(a));
 
-//   for (let i = 0; i < sortedDateArr.length; i += 1) {
-//     const preparedCardsArr = cards.filter(
-//       card => card.dayRead === sortedDateArr[i]
-//     );
-//     const cardMarkup = preparedCardsArr
-//       .map(card => createNewsCard(card))
-//       .join('');
-//   }
+      accGallery.insertAdjacentHTML('beforebegin', readMarkupCard)
+      return readMarkupCard;
+  // }
+    
+  }
 
-//   return markup;
-// }
