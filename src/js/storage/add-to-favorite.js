5405e742f3452de47bde61a47ee119146b95231b
Nechai_Ihor <fredduke1983@gmail.com>
2023-03-10 01:07:08 +0200

    up favorite

--- src/js/storage/add-to-favorite.js
+++ src/js/storage/add-to-favorite.js
@@ -1,49 +1,45 @@
-import axios from 'axios';
 import { getDataFromLocalStorage, setDataToLocalStorage } from '../api/service';
+import { getCard } from '../gallery';
 
 // ===========================================//
 
 const galleryEl = document.querySelector('.news__gallery');
 
-console.log(galleryEl);
-//////////////////////////////////////
-const POPULAR_NEWS_URL = `https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=ctrAXxxlZTZKIuOVxETyJyELWuuMaa5A`;
-async function fetchFavoriteNews() {
-  let { data } = await axios.get(POPULAR_NEWS_URL);
-
-  return data;
-}
-///////////////////////////////////
-
 if (galleryEl) galleryEl.addEventListener('click', onBtnFavoriteClick);
 
-let dataFavorite = null;
 export async function onBtnFavoriteClick(e) {
   try {
     const btn = e.target.closest('.item-news__add-to-favorite');
     if (!btn) return;
 
     btn.classList.toggle('hidden-span');
-    const idCard = btn.closest('.card').dataset.id;
 
+    const idCard = btn.closest('.card').dataset.id;
     const newsArray = getDataFromLocalStorage('news');
 
-    const indexElem = newsArray.findIndex(
-      ({ id }) => Number(id) === Number(idCard)
-    );
-    console.log('indexElem ', indexElem);
+    //!===============================
 
-    if (indexElem > -1) {
-      newsArray.splice(indexElem, 1);
-    } else {
-      if (!dataFavorite) {
-        const { results } = await fetchFavoriteNews();
-        dataFavorite = results;
-      }
-      const obj = dataFavorite.find(({ id }) => Number(id) === Number(idCard));
-      newsArray.push(obj);
+    const dataFavorite = getCard();
+
+    const oneCard = dataFavorite.find(
+      ({ id }) => String(id) === String(idCard)
+    );
+
+    if (newsArray.every(card => String(idCard) !== String(card.id))) {
+      newsArray.push(oneCard);
+      setDataToLocalStorage('news', newsArray);
     }
-    setDataToLocalStorage('news', newsArray);
+
+    if (!btn.classList.contains('hidden-span')) {
+      const element = newsArray.find(({ id }) => String(id) === String(idCard));
+
+      const indexForDel = Number(newsArray.indexOf(element));
+
+      newsArray.splice(indexForDel, 1);
+      setDataToLocalStorage('news', newsArray);
+    }
+
+    //!===============================
   } catch (error) {
     console.log(error.message);
   }
